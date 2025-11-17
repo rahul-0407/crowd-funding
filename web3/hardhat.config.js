99efe3d4a1e71f3545774c858a1374f834a60ac0
rahul-0407 <rahulkrgupta0407@gmail.com>
2025-11-17 22:58:58 +0530

    sidebar and navbar added

---
+++ web3/hardhat.config.js
@@ -0,0 +1,57 @@
+require("@matterlabs/hardhat-zksync-solc");
+require("@matterlabs/hardhat-zksync-verify");
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.4.1",
+    defaultNetwork: 'sepolia',
+    networks:{
+      hardhat:{},
+      sepolia:{
+        url: 'https://chain.instanodes.io/eth-testnet/?apikey=FWyJ1qbkVHcZgWjx',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      }
+    },
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    zkSyncSepoliaTestnet: {
+      url: "https://sepolia.era.zksync.dev",
+      ethNetwork: "sepolia",
+      zksync: true,
+      chainId: 300,
+      verifyURL:
+        "https://explorer.sepolia.era.zksync.dev/contract_verification",
+    },
+    zkSyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      chainId: 324,
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
